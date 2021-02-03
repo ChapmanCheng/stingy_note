5bf99b2431b06a317563791d23e2d0ea51cec884
chapmancheng <chapboy1989@gmail.com>
2021-02-03 21:23:52 +0800

    extracted lineargradient colors to another file

--- App.tsx
+++ App.tsx
@@ -2,14 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import {LinearGradient} from 'expo-linear-gradient'
-import { backgroundColor } from './utilities/colors';
+import { linearGradient } from './utilities/colors';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <LinearGradient
-        colors={backgroundColor}
-        locations={[0.05, 0.7, 1]}
+        colors={linearGradient.colors}
+        locations={linearGradient.locations}
         style={styles.linearBg}
       >
         <Text>Open up App.tsx to start working on your app!</Text>
--- utilities/colors.ts
+++ utilities/colors.ts
@@ -1,3 +1,8 @@
-const backgroundColor: string[] = ['#fff', 'rgba(239, 98, 227, 0.5)', 'rgba(60, 90, 193, 0.5)']
+import {LinearGradientProps} from 'expo-linear-gradient'
 
-export {backgroundColor}
+const linearGradient: LinearGradientProps = {
+    colors: ['#fff', 'rgba(239, 98, 227, 0.5)', 'rgba(60, 90, 193, 0.5)'],
+    locations: [0.05, 0.7, 1]
+}
+
+export {linearGradient}
