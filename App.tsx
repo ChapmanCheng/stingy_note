a1de5833fe30560b5856ed90dd114e84719b3170
chapmancheng <chapboy1989@gmail.com>
2021-02-03 21:05:12 +0800

    added linear-gradient-background

--- App.tsx
+++ App.tsx
@@ -1,21 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {LinearGradient} from 'expo-linear-gradient'
+import { backgroundColor } from './utilities/colors';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <LinearGradient
+        colors={backgroundColor}
+        locations={[0.05, 0.7, 1]}
+        style={styles.linearBg}
+      >
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </LinearGradient>
     </View>
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'stretch',
     justifyContent: 'center',
   },
+
+  linearBg: {
+    flex: 1, 
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
 });
---
+++ utilities/colors.ts
@@ -0,0 +1,3 @@
+const backgroundColor: string[] = ['#fff', 'rgba(239, 98, 227, 0.5)', 'rgba(60, 90, 193, 0.5)']
+
+export {backgroundColor}
