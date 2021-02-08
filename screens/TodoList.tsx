27e685fc9dbb00d265086d4b24dbedfa4e942ec1
chapmancheng <chapboy1989@gmail.com>
2021-02-09 01:04:59 +0800

    added TodoList.tsx component

---
+++ screens/TodoList.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import LinearGradientBackground from '../utilities/LinearGradientBackground'
+
+export default function todoList() {
+    return (
+        <LinearGradientBackground>
+            <Text>TodoList</Text>
+        </LinearGradientBackground>
+        
+    )
+}
+
+const styles = StyleSheet.create({})
