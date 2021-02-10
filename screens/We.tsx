8a81a50d5812f8cf1be688dc6be3bbb68d375f0e
chapmancheng <chapboy1989@gmail.com>
2021-02-11 01:39:42 +0800

    semi complete We component

--- screens/We.tsx
+++ screens/We.tsx
@@ -1,14 +1,81 @@
 import React from 'react'
-import { StyleSheet, Text, View, Image } from 'react-native'
+import { StyleSheet, Text, View, Image, StyleProp, ViewStyle } from 'react-native'
+import { TextInput } from 'react-native-gesture-handler'
+import globalStyles from '../styles/global'
 import LinearGradientBackground from '../utilities/LinearGradientBackground'
 
 export default function We() {
     return (   
         <LinearGradientBackground>
-            <Image source={require('../assets/boyface.png')} />
-            <Image source={require('../assets/girlface.png')} />
+            <View style={globalStyles.inputGroup}>
+                <Image source={require('../assets/boyface.png')} />
+                <TextInput style={styles.longerInput}/>
+            </View>
+            <View style={globalStyles.inputGroup}>                
+                <Image source={require('../assets/girlface.png')} />
+                <TextInput style={styles.longerInput}/>
+            </View>
+            <View style={globalStyles.inputGroup}>
+                <Text style={globalStyles.label}>拍拖紀念日</Text>
+                <TextInput style={globalStyles.input}/>
+            </View>
+            <Text style={styles.comment}>~嘩！10週年</Text>
+            <View style={globalStyles.inputGroup}>
+                <Text style={globalStyles.label}>拍拖紀念日</Text>
+                <TextInput style={globalStyles.input}/>
+            </View>
+            <Text style={styles.comment}>~好sweet呀~</Text>
+
+            {/* ~~~~~~~~~~ JUST FOR VISUAL ~~~~~~~~~~ */}
+            
+            <View style={styles.inputGroup2x1}>
+                <View style={styles.inputBlock}>
+                    <Text style={globalStyles.label}>幾時表白?</Text>
+                    <TextInput style={styles.shorterInput}/>
+                </View>
+                <View style={styles.inputBlock}>
+                    <Text style={globalStyles.label}>喺邊表白?</Text>
+                    <TextInput style={styles.shorterInput}/>
+                </View>
+            </View>
+            <View style={styles.inputGroup2x1}>
+                <View style={styles.inputBlock}>
+                    <Text style={globalStyles.label}>幾時初吻?</Text>
+                    <TextInput style={styles.shorterInput}/>
+                </View>
+                <View style={styles.inputBlock}>
+                    <Text style={globalStyles.label}>初吻喺邊?</Text>
+                    <TextInput style={styles.shorterInput}/>
+                </View>
+            </View>
+            <Text style={styles.comment}>{"寫低就唔會忘記啦^_<"}</Text>
+            
+
+
+
         </LinearGradientBackground>
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    inputGroup2x1: {
+        ...globalStyles.inputGroup,
+        justifyContent: 'center',
+        marginBottom: 24
+    },
+    longerInput: {
+        ...globalStyles.input,
+        width: 265
+    },
+    shorterInput: {
+        ...globalStyles.input,
+        width: 145
+    },
+    inputBlock: {alignItems: 'center', marginHorizontal: 16},
+    comment: {
+        textAlign: 'center',
+        fontSize: 22,
+        marginBottom: 24,
+        fontWeight: 'bold'
+    }
+})
