914b5b6ca3ffe859c26f155d4c1b761c9f2d4ef7
chapmancheng <chapboy1989@gmail.com>
2021-02-12 23:41:43 +0800

    added 小事情 大計劃 title

--- screens/TodoList.tsx
+++ screens/TodoList.tsx
@@ -1,13 +1,22 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image, Dimensions } from "react-native";
 import LinearGradientBackground from "../utilities/LinearGradientBackground";
 
 export default function todoList() {
   return (
     <LinearGradientBackground>
-      <Text>TodoList</Text>
+      <View style={{alignItems: 'center'}}>
+
+      <Image style={styles.imgTitle} width={Dimensions.get('window').width}  source={require('../assets/todoListTitle.png')}/>
+      </View>
+      
     </LinearGradientBackground>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  imgTitle: {
+    // width: Dimensions.get('window').width,
+    // height: 250
+  }
+});
