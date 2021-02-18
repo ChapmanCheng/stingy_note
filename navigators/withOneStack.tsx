b0fdfe2d1ce4da2aadd13996a25317904c9203ed
chapmancheng <chapboy1989@gmail.com>
2021-02-18 17:33:20 +0800

    added custom header to withOneStack component

--- navigators/withOneStack.tsx
+++ navigators/withOneStack.tsx
@@ -5,13 +5,14 @@ import {
   StackNavigationOptions,
 } from "@react-navigation/stack";
 import { Ionicons } from "@expo/vector-icons";
-import { StyleSheet } from "react-native";
+import { StyleSheet, Text } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 
 const { Navigator, Screen } = createStackNavigator();
 
 export default function withOneStack(WrappedComponent: React.FC) {
   return () => (
-    <Navigator screenOptions={screenOptions}>
+    <Navigator screenOptions={screenOptions} headerMode={"screen"}>
       <Screen name={"愛情小氣簿"} component={WrappedComponent} />
     </Navigator>
   );
@@ -23,19 +24,57 @@ interface screenOptionsProps {
 }
 
 const styles = StyleSheet.create({
-  header: {},
+  header: {
+    minHeight: 42,
+    marginHorizontal: 10,
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderRadius: 10,
+  },
+  headerText: {
+    fontSize: 24,
+    color: "#fff",
+  },
+  headerMenu: {
+    color: "#fff",
+  },
 });
 
 const screenOptions = ({
   navigation,
   route,
 }: screenOptionsProps): StackNavigationOptions => ({
-  headerLeft: () => (
+  header: ({}) => (
+      <LinearGradient
+        colors={["rgba(243, 229, 191, 0.47)", "rgba(243, 137, 130, 1)"]}
+        start={{ x: 0, y: 0 }}
+        end={{ x: 1, y: 1 }}
+        style={styles.header}
+      >
+        <Text style={styles.headerText}>愛情小氣簿</Text>
+
     <Ionicons
       name="menu"
       onPress={() => navigation.openDrawer()}
-      size={30}
-      style={{ marginLeft: 16 }}
+          size={35}
+          style={styles.headerMenu}
     />
+      </LinearGradient>
   ),
+
+  // headerLeft: () => (
+  //   <Ionicons
+  //     name="menu"
+  //     onPress={() => navigation.openDrawer()}
+  //     size={30}
+  //     style={{ marginLeft: 16 }}
+  //   />
+  // ),
 });
+
+// colors={["rgba(41, 186, 218, 0.91)", "rgba(237, 66, 148, 0.83)"]}
+// start={{x: 0, y: 0}}
+// end={{x: 1, y: 1}}
