df1c9e967e53b44c6edb94fafcd41875d88b063c
chapmancheng <chapboy1989@gmail.com>
2021-03-01 19:35:34 +0800

    delete unecessary import

--- navigators/MainStackNavigator.tsx
+++ navigators/MainStackNavigator.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import Home from "../screens/StingyNotes";
-import About from "../screens/About";
 
 const { Navigator, Screen } = createStackNavigator();
 
